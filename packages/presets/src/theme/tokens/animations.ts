3a6f9f7d5304f45feeab21634609c37d2aa6d7f3
Christian Schröter <dev@grizzly.codes>
2023-08-04 23:20:44 +0200

    chore(presets): use curly notation

--- packages/presets/src/theme/tokens/animations.ts
+++ packages/presets/src/theme/tokens/animations.ts
@@ -8,27 +8,27 @@ export const easings = defineTokens.easings({
 
 export const animations = defineTokens.animations({
   'backdrop-in': {
-    value: 'fade-in 250ms var(--easings-default)',
+    value: 'fade-in 250ms {easings.default}',
   },
   'backdrop-out': {
-    value: 'fade-out 200ms var(--easings-default)',
+    value: 'fade-out 200ms {easings.default}',
   },
   'dialog-in': {
-    value: 'slide-in 400ms var(--easings-emphasized-in)',
+    value: 'slide-in 400ms {easings.emphasized-in}',
   },
   'dialog-out': {
-    value: 'slide-out 200ms var(--easings-emphasized-out)',
+    value: 'slide-out 200ms {easings.emphasized-out}',
   },
   'drawer-in-left': {
-    value: 'slide-in-left 400ms var(--easings-emphasized-in)',
+    value: 'slide-in-left 400ms {easings.emphasized-in}',
   },
   'drawer-out-left': {
-    value: 'slide-out-left 200ms var(--easings-emphasized-out)',
+    value: 'slide-out-left 200ms {easings.emphasized-out}',
   },
   'drawer-in-right': {
-    value: 'slide-in-right 400ms var(--easings-emphasized-in)',
+    value: 'slide-in-right 400ms {easings.emphasized-in}',
   },
   'drawer-out-right': {
-    value: 'slide-out-right 200ms var(--easings-emphasized-out)',
+    value: 'slide-out-right 200ms {easings.emphasized-out}',
   },
 })
