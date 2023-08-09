51c717c8f797c8ac1d9ee51a6e94d52fda672ff7
Christian Schröter <dev@grizzly.codes>
2023-08-09 19:44:56 +0200

    chore: improve cli

--- packages/cli/src/generate-component.ts
+++ packages/cli/src/generate-component.ts
@@ -1,42 +1,32 @@
-import { camelCase, chain, lowerCase } from 'voca'
+import { camelCase, chain } from 'voca'
 
-const startsWithUpperCase = (str: string) => chain(str).first().isUpperCase().value()
+export const startsWithUpperCase = (str: string) => chain(str).first().isUpperCase().value()
 const pascalCase = (s: string) => chain(s).camelCase().capitalize().value()
 
-const getComponents = async (moduleName: string) => {
-  const ArkComponent = await import(`@ark-ui/react/${moduleName}`).then(
-    (m) => m[pascalCase(moduleName)],
-  )
-  return Object.keys(ArkComponent).filter(startsWithUpperCase)
-}
-
-const getParts = async (moduleName: string) => {
-  const anatomyName = `${camelCase(moduleName)}Anatomy`
-  const anatomy = await import(`@ark-ui/react/${moduleName}`).then((m) => m[anatomyName])
-  return anatomy.keys() as string[]
-}
+const getComponent = async (moduleName: string) =>
+  import(`@ark-ui/react/${moduleName}`).then((m) => m[pascalCase(moduleName)])
 
 export const generateComponent = async (moduleName: string) => {
-  const components = await getComponents(moduleName)
-  const parts = await getParts(moduleName)
+  const component = await getComponent(moduleName)
 
   return {
     [moduleName]: {
-      components: components.reduce(
-        (acc, componentName) => {
-          const partName = parts.find((partName) => lowerCase(componentName).endsWith(partName))
+      components: Object.entries(component)
+        .filter(([key]) => startsWithUpperCase(key))
+        .reduce((acc, [key, value]) => {
           return {
             ...acc,
-            [componentName]: {
-              name: pascalCase(moduleName).concat(componentName),
-              partName,
+            [key]: {
+              // @ts-expect-error
+              name: value.displayName ?? pascalCase(moduleName).concat(key === 'Root' ? '' : key),
+              partName: camelCase(key),
             },
           }
-        },
-        {} as Record<string, { name: string; partName?: string }>,
-      ),
+        }, {}),
       isArkComponent: true,
       rootComponent: pascalCase(moduleName),
+      // Switch is a reserved word
+      className: moduleName === 'switch' ? 'switchRecipe' : camelCase(moduleName),
     },
   }
 }
