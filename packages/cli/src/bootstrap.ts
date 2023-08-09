1708344319cba70bdb7e23888664d0e84471ce51
Christian Schröter <dev@grizzly.codes>
2023-08-09 12:00:11 +0200

    chore: camelcase

--- packages/cli/src/bootstrap.ts
+++ packages/cli/src/bootstrap.ts
@@ -1,9 +1,11 @@
 import { outputFile, readFileSync } from 'fs-extra'
 import Handlebars from 'handlebars'
+import { camelCase } from 'voca'
 import data from '../components.json'
 
 const main = async () => {
   Handlebars.registerHelper('eq', (a, b) => a === b)
+  Handlebars.registerHelper('camelCase', camelCase)
   const template = Handlebars.compile(readFileSync('./src/react.hbs', 'utf-8'))
 
   await Promise.all(
