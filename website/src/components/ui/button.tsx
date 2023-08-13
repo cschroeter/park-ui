82408d959418df5ae768ff9436a40c7164c8e3ac
Christian Schröter <dev@grizzly.codes>
2023-08-13 11:56:08 +0200

    feat: use ark factory to allow for some nice composition

--- website/src/components/ui/button.tsx
+++ website/src/components/ui/button.tsx
@@ -1,6 +1,8 @@
+'use client' // TODO this should not be necessary
+import { ark } from '@ark-ui/react'
 import type { ComponentPropsWithoutRef } from 'react'
 import { styled } from 'styled-system/jsx'
 import { button, type ButtonVariantProps } from 'styled-system/recipes'
 
-export type ButtonProps = ButtonVariantProps & ComponentPropsWithoutRef<'button'>
-export const Button = styled('button', button)
+export type ButtonProps = ButtonVariantProps & ComponentPropsWithoutRef<typeof ark.button>
+export const Button = styled(ark.button, button)
