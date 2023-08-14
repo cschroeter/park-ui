255a205e3f4d0ecea1634ada4ed1638fe6643e1d
Christian Schröter <dev@grizzly.codes>
2023-08-14 19:34:28 +0200

    chore: wip

--- website/src/components/marketing/theme-generator.tsx
+++ website/src/components/marketing/theme-generator.tsx
@@ -1,8 +1,7 @@
-import { BiUndo } from 'react-icons/bi'
-import { Box, Stack } from 'styled-system/jsx'
+import { Box, HStack, Stack } from 'styled-system/jsx'
 import { Heading, Typography } from '~/components/ui/typography'
 import { useThemeGenerator } from '~/lib/use-theme-generator'
-import { IconButton } from '../ui/icon-button'
+import { Button } from '../ui/button'
 import { BorderRadiusSlider } from './theme/border-radius-slider'
 import { ColorPalettePicker } from './theme/color-palette-picker'
 import { FontFamilySelect } from './theme/font-family-select'
@@ -21,27 +20,25 @@ export const ThemeGenerator = () => {
       width="sm"
       position="relative"
     >
-      <Stack gap="5">
-        <Box position="absolute" top="2" right="2">
-          <IconButton
-            aria-label="Reset styles"
-            variant="tertiary"
-            onClick={() => reset()}
-            size="sm"
-          >
-            <BiUndo />
-          </IconButton>
-        </Box>
-        <Stack gap="1">
-          <Heading as="h3">Make it yours</Heading>
-          <Typography color="fg.muted" textStyle="sm">
-            Pick a style and color for your components.
-          </Typography>
+      <Stack gap="8">
+        <Stack gap="5">
+          <Stack gap="1">
+            <Heading as="h3">Make it yours</Heading>
+            <Typography color="fg.muted" textStyle="sm">
+              Pick a style and color for your components.
+            </Typography>
+          </Stack>
+          <FontFamilySelect />
+          <GrayPalettePicker />
+          <ColorPalettePicker />
+          <BorderRadiusSlider />
         </Stack>
-        <FontFamilySelect />
-        <GrayPalettePicker />
-        <ColorPalettePicker />
-        <BorderRadiusSlider />
+        <HStack gap="3">
+          <Button variant="secondary" width="full" onClick={() => reset()}>
+            Reset
+          </Button>
+          <Button width="full">Copy Code</Button>
+        </HStack>
       </Stack>
     </Box>
   )
