584226d1b822e7e6342607364808e795d0262eb4
Brian Cooper <brian@brian-cooper.com>
2023-08-29 18:19:40 -0500

    refactor(code): convert wrapped 'div' tag to semantic 'code' tag

--- website/src/components/ui/code.tsx
+++ website/src/components/ui/code.tsx
@@ -4,4 +4,4 @@ import { styled } from 'styled-system/jsx'
 import { code, type CodeVariantProps } from 'styled-system/recipes'
 
 export type CodeProps = CodeVariantProps & ComponentPropsWithoutRef<typeof ark.code>
-export const Code = styled(ark.div, code)
+export const Code = styled(ark.code, code)
