72010999d1e2eb7aba1508e53aa5feef8f859e0c
Christian Schröter <dev@grizzly.codes>
2023-08-29 20:30:12 +0200

    chore: wip

---
+++ examples/vue/nuxt/app.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Tabs, TabList, TabTrigger, TabIndicator, TabContent } from '@ark-ui/vue'
+</script>
+
+<template>
+  <Tabs defaultValue="react">
+    <TabList>
+      <TabTrigger value="react"><button>React</button></TabTrigger>
+      <TabTrigger value="solid"><button>Solid</button></TabTrigger>
+      <TabTrigger value="vue"><button>Vue</button></TabTrigger>
+      <TabIndicator />
+    </TabList>
+    <TabContent value="react">React</TabContent>
+    <TabContent value="solid">Solid</TabContent>
+    <TabContent value="vue">Vue</TabContent>
+  </Tabs>
+</template>
---
+++ examples/vue/nuxt/panda.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from '@pandacss/dev'
+
+export default defineConfig({
+  // Whether to use css reset
+  preflight: true,
+
+  // Where to look for your css declarations
+  include: ['./components/**/*.{js,jsx,ts,tsx,vue}', './pages/**/*.{js,jsx,ts,tsx,vue}'],
+
+  // Files to exclude
+  exclude: [],
+
+  // Useful for theme customization
+  theme: {
+    extend: {},
+  },
+
+  // The output directory for your css system
+  outdir: 'styled-system',
+})
