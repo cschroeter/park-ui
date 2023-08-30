d238257c82f209679e7b6bb4eedd259e891011a3
Christian Schröter <dev@grizzly.codes>
2023-08-30 10:15:41 +0200

    feat(website): simplify examples tabs

--- website/src/components/marketing/examples.tsx
+++ website/src/components/marketing/examples.tsx
@@ -1,13 +1,10 @@
 'use client'
-import { MenuSeparator, Portal } from '@ark-ui/react'
 import { useState } from 'react'
-import { FiArrowRight, FiExternalLink, FiMoreVertical } from 'react-icons/fi'
+import { FiArrowRight } from 'react-icons/fi'
 import { css, cx } from 'styled-system/css'
-import { Container, HStack, Spacer } from 'styled-system/jsx'
+import { Container, Spacer } from 'styled-system/jsx'
 import { button } from 'styled-system/recipes'
 import { TabContent, TabIndicator, TabList, TabTrigger, Tabs } from '~/components/ui/tabs'
-import { Button } from '../ui/button'
-import { Menu, MenuContent, MenuItem, MenuPositioner, MenuTrigger } from '../ui/menu'
 import { Authentication } from './examples/authentication'
 import { Cards } from './examples/cards'
 import { ECommerce } from './examples/e-commerce'
@@ -43,11 +40,7 @@ export const Examples = () => {
       <Tabs value={activeTab} onChange={(e) => setActiveTab(e.value!)}>
         <TabList>
           {examples.map((example, id) => (
-            <TabTrigger
-              key={example.value}
-              value={example.value}
-              display={{ base: id > 1 ? 'none' : 'initial', md: 'initial' }}
-            >
+            <TabTrigger key={example.value} value={example.value}>
               {example.label}
             </TabTrigger>
           ))}
@@ -63,47 +56,6 @@ export const Examples = () => {
           >
             Get Code <FiArrowRight />
           </a>
-          <Menu size="xs" positioning={{ placement: 'bottom-end' }}>
-            <MenuTrigger asChild>
-              <Button
-                aria-label="Open Menu"
-                variant="tertiary"
-                size="sm"
-                px="0"
-                display={{ md: 'none' }}
-                transform="translateY(-0.25rem)"
-              >
-                <FiMoreVertical />
-              </Button>
-            </MenuTrigger>
-            <Portal>
-              <MenuPositioner>
-                <MenuContent minW="10rem">
-                  {examples.slice(2).map((example) => (
-                    <MenuItem
-                      id={example.value}
-                      key={example.value}
-                      onClick={() => setActiveTab(example.value)}
-                    >
-                      {example.label}
-                    </MenuItem>
-                  ))}
-                  <MenuSeparator />
-                  <MenuItem id="examples" asChild>
-                    <a
-                      href={`https://github.com/cschroeter/park-ui/tree/main/website/src/components/marketing/examples/${activeTab}`}
-                      target="_blank"
-                    >
-                      <HStack justify="space-between" width="full">
-                        Get Code
-                        <FiExternalLink />
-                      </HStack>
-                    </a>
-                  </MenuItem>
-                </MenuContent>
-              </MenuPositioner>
-            </Portal>
-          </Menu>
         </TabList>
         {examples.map((example) => (
           <TabContent key={example.value} value={example.value} lazyMount unmountOnExit>
