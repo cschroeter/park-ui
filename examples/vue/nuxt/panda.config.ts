5048af5e1f77f7942f7a21a52b93e75402d70768
Christian Schröter <dev@grizzly.codes>
2023-08-30 23:25:10 +0200

    chore: wip

--- examples/vue/nuxt/panda.config.ts
+++ examples/vue/nuxt/panda.config.ts
@@ -1,20 +1,10 @@
 import { defineConfig } from '@pandacss/dev'
 
 export default defineConfig({
-  // Whether to use css reset
   preflight: true,
-
-  // Where to look for your css declarations
+  presets: ['@pandacss/dev/presets', '@park-ui/presets'],
   include: ['./components/**/*.{js,jsx,ts,tsx,vue}', './pages/**/*.{js,jsx,ts,tsx,vue}'],
-
-  // Files to exclude
   exclude: [],
-
-  // Useful for theme customization
-  theme: {
-    extend: {},
-  },
-
-  // The output directory for your css system
   outdir: 'styled-system',
+  jsxFramework: 'vue',
 })
