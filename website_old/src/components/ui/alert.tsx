08eb7ae161ed37e3516073d76410b8608832f393
Christian Schröter <dev@grizzly.codes>
2023-09-29 23:36:24 +0200

    chore: wip

--- website_old/src/components/ui/alert.tsx
+++
@@ -1,7 +0,0 @@
-import { ark } from '@ark-ui/react'
-import type { ComponentPropsWithoutRef } from 'react'
-import { styled } from 'styled-system/jsx'
-import { alert, type AlertVariantProps } from 'styled-system/recipes'
-
-export type AlertProps = AlertVariantProps & ComponentPropsWithoutRef<typeof ark.div>
-export const Alert = styled(ark.div, alert)
--- website_old/src/components/ui/badge.tsx
+++
@@ -1,7 +0,0 @@
-import { ark } from '@ark-ui/react'
-import type { ComponentPropsWithoutRef } from 'react'
-import { styled } from 'styled-system/jsx'
-import { badge, type BadgeVariantProps } from 'styled-system/recipes'
-
-export type BadgeProps = BadgeVariantProps & ComponentPropsWithoutRef<typeof ark.div>
-export const Badge = styled(ark.div, badge)
