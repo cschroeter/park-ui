c7b86206ec5b6bfb02dfe6503e2c2ca2a102f4b5
Christian Schröter <dev@grizzly.codes>
2023-10-09 20:59:17 +0200

    chore: wip

--- packages/panda/src/theme/tokens/helpers/define-color-palettes.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from 'bun:test'
-import { defineColorPalettes } from './define-color-palettes'
-
-test('create color token test', () => {
-  const colorPalettes = defineColorPalettes({ grayColor: 'neutral' })
-  expect(colorPalettes).toMatchSnapshot()
-})
