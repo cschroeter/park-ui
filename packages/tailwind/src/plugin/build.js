da597a3aebdb2d655c3e901f357dd3a4ee08162e
Simon Holzmayer <dev@holzmayer.me>
2023-10-18 23:06:48 +0200

    fix: remove tailwind decorators from preset

--- packages/tailwind/src/plugin/build.js
+++ packages/tailwind/src/plugin/build.js
@@ -3,7 +3,8 @@ const postcss = require('postcss')
 const postcssJs = require('postcss-js')
 
 const cssInJs = readStylesToCssInJs()
-writeFormattedStylesJson(cssInJs)
+const cssInJsWithoutLayers = removePandaLayers(cssInJs)
+writeFormattedStylesJson(cssInJsWithoutLayers)
 
 function writeFormattedStylesJson(cssInJs) {
   writeFileSync('./src/plugin/css/styles.json', JSON.stringify(cssInJs, null, 2))
@@ -17,4 +18,16 @@ function readStylesToCssInJs() {
   const root = postcss.parse(css)
   const cssInJs = postcssJs.objectify(root)
   return cssInJs
+}
+
+/**
+ * The panda theme has a lot of layers that we don't need in the plugin.
+ * They also cause problems with the tailwind setup.
+ */
+function removePandaLayers(cssInJs) {
+  const layers = ['base', 'utilities', 'recipes.slots', 'recipes', 'tokens']
+  return layers.reduce((result, layer) => {
+    const styles = cssInJs[`@layer ${layer}`]
+    return styles ? { ...result, ...styles } : result
+  }, {})
 }
