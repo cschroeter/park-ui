071603d034106977c0d502e0dde5b2230613d500
Simon Holzmayer <dev@holzmayer.me>
2023-10-19 22:04:45 +0200

    feat(examples): add astro example

---
+++ examples/tailwind/react/astro/astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
+import { astroImageTools } from 'astro-imagetools';
+
+// https://astro.build/config
+export default defineConfig({
+  // base: '.', // Set a path prefix.
+  site: 'https://example.com/', // Use to generate your sitemap and canonical URLs in your final build.
+  trailingSlash: 'always', // Use to always append '/' at end of url
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'monokai',
+    },
+  },
+  integrations: [
+    react(),
+    tailwind({}),
+    sitemap(),
+    robotsTxt(),
+    astroImageTools,
+  ],
+});
--- packages/tailwind/README.md
+++ packages/tailwind/README.md
@@ -1,16 +1,3 @@
-# Build plugin
-```bash
-bun build:plugin
-```
+# Park UI Tailwind CSS Plugin
 
-## Generate components
-```bash
-cd packages/tailwind && bun run build:stories && bun run build:components && bun run build:styles && git add . && cd ../.. && bun lint-staged
-```
-
-## Todos
-- add static components to panda.config.js that they are available in built file
-- publish plugin to npm
-- website: snippet.tsx for Icon is not shown (because it is not a ts file) -> TODO rename to tsx
-- publish lib files
-- click freezes all buttons
+To get started, see more information at [Park UI](https://park-ui.com/docs/tailwind/overview/getting-started/).
