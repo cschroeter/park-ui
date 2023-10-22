f949a80cda82f7879ad536fb71284199ec2e3875
Christian Schröter <dev@grizzly.codes>
2023-10-22 14:29:55 +0200

    feat(examples): update panda related examples

--- examples/panda/react/astro/panda.config.ts
+++ examples/panda/react/astro/panda.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@pandacss/dev'
 
 export default defineConfig({
   preflight: true,
-  presets: ['@pandacss/dev/presets', '@park-ui/presets'],
+  presets: ['@pandacss/preset-base', '@park-ui/panda-preset'],
   include: ['./src/**/*.{ts,tsx,js,jsx,astro}', './pages/**/*.{ts,tsx,js,jsx,astro}'],
   exclude: [],
   theme: {
