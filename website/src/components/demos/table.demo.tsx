cf017ff4018dec6c20779315c59646ff79a73dfa
Christian Schröter <dev@grizzly.codes>
2023-10-25 17:45:01 +0200

    chore: wip

--- website/src/components/demos/table.demo.tsx
+++ website/src/components/demos/table.demo.tsx
@@ -8,7 +8,6 @@ export const Demo = (props: TableProps) => {
         <Table.Row>
           <Table.Head>ID</Table.Head>
           <Table.Head>Name</Table.Head>
-          <Table.Head>Category</Table.Head>
           <Table.Head>Stock</Table.Head>
           <Table.Head textAlign="right">Price</Table.Head>
         </Table.Row>
@@ -18,7 +17,6 @@ export const Demo = (props: TableProps) => {
           <Table.Row key={index}>
             <Table.Cell fontWeight="medium">{product.id}</Table.Cell>
             <Table.Cell>{product.name}</Table.Cell>
-            <Table.Cell>{product.category}</Table.Cell>
             <Table.Cell>{product.stock}</Table.Cell>
             <Table.Cell textAlign="right">{product.price}</Table.Cell>
           </Table.Row>
@@ -26,7 +24,7 @@ export const Demo = (props: TableProps) => {
       </Table.Body>
       <Table.Footer>
         <Table.Row>
-          <Table.Cell colSpan={3}>Totals</Table.Cell>
+          <Table.Cell colSpan={2}>Totals</Table.Cell>
           <Table.Cell>87</Table.Cell>
           <Table.Cell textAlign="right">$34,163.00</Table.Cell>
         </Table.Row>
@@ -36,7 +34,7 @@ export const Demo = (props: TableProps) => {
 }
 
 const productData = [
-  { id: 'P001', name: 'MacBook Pro', category: 'Electronics', stock: 12, price: '$1999.00' },
-  { id: 'P002', name: 'AirPods Pro', category: 'Audio', stock: 25, price: '$249.00' },
-  { id: 'P003', name: 'Leather Wallet', category: 'Accessories', stock: 50, price: '$79.00' },
+  { id: 'P001', name: 'MacBook Pro', stock: 12, price: '$1999.00' },
+  { id: 'P002', name: 'AirPods Pro', stock: 25, price: '$249.00' },
+  { id: 'P003', name: 'Leather Wallet', stock: 50, price: '$79.00' },
 ]
