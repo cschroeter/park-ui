3f6e0ed980d5eee5c67f61cb6ed32c9efb3505ff
Christian Schröter <dev@grizzly.codes>
2023-11-06 08:03:16 +0100

    chore: disable examples from vscode search

--- packages/panda/src/theme/recipes/link.ts
+++ packages/panda/src/theme/recipes/link.ts
@@ -11,7 +11,7 @@ export const link = defineRecipe({
     gap: '2',
     textDecoration: 'underline 0.1em transparent',
     textUnderlineOffset: '0.125em',
-    transitionDuration: 'fast',
+    transitionDuration: 'normal',
     transitionProperty: 'text-decoration-color',
     transitionTimingFunction: 'default',
     _hover: {
