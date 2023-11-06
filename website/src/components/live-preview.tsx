03c7148b054643f4d8975a3be9b6993f445926f6
Brian Cooper <20056195+coopbri@users.noreply.github.com>
2023-11-06 02:08:58 -0600

    feat(checkbox): add styles for indeterminate state (#79)

--- website/src/components/live-preview.tsx
+++ website/src/components/live-preview.tsx
@@ -2,9 +2,10 @@ import { ExternalLinkIcon, InfoIcon } from 'lucide-react'
 import { generateElement } from 'react-live'
 import { Box, Flex, HStack, Stack } from 'styled-system/jsx'
 import * as components from '~/components/ui'
+import { Demo } from './demos/checkbox.demo'
 
 const icons = { InfoIcon, ExternalLinkIcon }
-const scope = { ...components, ...icons, Stack, HStack, Box, Flex }
+const scope = { ...components, ...icons, Stack, HStack, Box, Flex, Checkbox: Demo }
 
 type Props = {
   code: string
