827261192461e814e49f803335f0a5fba1528100
Christian Schröter <dev@grizzly.codes>
2023-11-08 22:48:46 +0100

    feat(cli): extend initial setup to use tsx

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,4 +1,24 @@
-const main = async () => {
-  console.log('hello from park-ui!')
-}
-main()
+import { intro, outro, select } from '@clack/prompts'
+
+intro(`Welcome to Park UI!`)
+
+const cssFramework = await select({
+  message: 'Which CSS framework would you like to use?',
+  options: [
+    { value: 'panda', label: 'Panda CSS' },
+    { value: 'tailwind', label: 'Tailwind CSS' },
+  ],
+})
+
+const jsFramework = await select({
+  message: 'Which JS framework would you like to use?',
+  options: [
+    { value: 'react', label: 'React' },
+    { value: 'solid', label: 'Solid' },
+    { value: 'vue', label: 'Vue' },
+  ],
+})
+
+console.log(cssFramework, jsFramework)
+
+outro(`You're all set!!`)
