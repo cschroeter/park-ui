da4e3966c2e15116a857ee121c753372df503769
Christian Schröter <dev@grizzly.codes>
2023-11-09 14:04:44 +0100

    feat(examples): update panda react gatsby example

---
+++ examples/panda/react/gatsby/src/components/ui/toggle-group.tsx
@@ -0,0 +1,17 @@
+import * as Ark from '@ark-ui/react/toggle-group'
+import { styled } from 'styled-system/jsx'
+import { toggleGroup, type ToggleGroupVariantProps } from 'styled-system/recipes'
+import { createStyleContext } from '~/lib/create-style-context'
+
+const { withProvider, withContext } = createStyleContext(toggleGroup)
+
+export * from '@ark-ui/react/toggle-group'
+export type ToggleGroupProps = Ark.ToggleGroupProps & ToggleGroupVariantProps
+
+const ToggleGroupRoot = withProvider(styled(Ark.ToggleGroup.Root), 'root')
+export const ToggleGroupItem = withContext(styled(Ark.ToggleGroup.Item), 'item')
+
+export const ToggleGroup = Object.assign(ToggleGroupRoot, {
+  Root: ToggleGroupRoot,
+  Item: ToggleGroupItem,
+})
