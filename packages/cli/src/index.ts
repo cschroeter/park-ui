4c71a99c7f11e96fea8871d8e1c4639756ac5206
Simon Holzmayer <dev@holzmayer.me>
2023-11-09 19:01:04 +0100

    feat: show version in cli

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,10 +1,14 @@
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 import { initCommand } from './commands/init'
+import { getVersion } from './commands/version'
 
 const main = async () => {
   yargs(hideBin(process.argv))
     .command('init', 'Initialize a new Park UI project', initCommand)
+    .example('$0 init', 'Initialize a new Park UI project')
+    .describe('init', 'Initialize a new Park UI project')
+    .version(`Park UI CLI version ${await getVersion()}`)
     .demandCommand(1)
     .strictCommands().argv
 }
