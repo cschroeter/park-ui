f621bc2d2cc2ef615bd88bcc85d4b1835717b6d5
Christian Schröter <dev@grizzly.codes>
2023-11-09 13:43:38 +0100

    feat(examples): update panda react astro example

--- examples/panda/react/astro/src/components/ui/button.tsx
+++ examples/panda/react/astro/src/components/ui/button.tsx
@@ -1,6 +1,6 @@
-import type { ComponentPropsWithoutRef } from 'react'
+import { ark } from '@ark-ui/react'
 import { styled } from 'styled-system/jsx'
-import { button, type ButtonVariantProps } from 'styled-system/recipes'
+import { button } from 'styled-system/recipes'
 
-export type ButtonProps = ButtonVariantProps & ComponentPropsWithoutRef<'button'>
-export const Button = styled('button', button)
+export type ButtonProps = typeof Button
+export const Button = styled(ark.button, button)
--- examples/panda/react/astro/src/components/ui/combobox.tsx
+++ examples/panda/react/astro/src/components/ui/combobox.tsx
@@ -6,20 +6,26 @@ import { createStyleContext } from '~/lib/create-style-context'
 const { withProvider, withContext } = createStyleContext(combobox)
 
 export * from '@ark-ui/react/combobox'
-export type ComboboxProps = Ark.ComboboxProps & ComboboxVariantProps
+export type ComboboxProps<T extends Ark.CollectionItem> = Ark.ComboboxProps<T> &
+  ComboboxVariantProps
 
 const ComboboxRoot = withProvider(styled(Ark.Combobox.Root), 'root')
 export const ComboboxClearTrigger = withContext(styled(Ark.Combobox.ClearTrigger), 'clearTrigger')
 export const ComboboxContent = withContext(styled(Ark.Combobox.Content), 'content')
 export const ComboboxControl = withContext(styled(Ark.Combobox.Control), 'control')
 export const ComboboxInput = withContext(styled(Ark.Combobox.Input), 'input')
-export const ComboboxLabel = withContext(styled(Ark.Combobox.Label), 'label')
-export const ComboboxOption = withContext(styled(Ark.Combobox.Option), 'option')
-export const ComboboxOptionGroup = withContext(styled(Ark.Combobox.OptionGroup), 'optionGroup')
-export const ComboboxOptionGroupLabel = withContext(
-  styled(Ark.Combobox.OptionGroupLabel),
-  'optionGroupLabel',
+export const ComboboxItem = withContext(styled(Ark.Combobox.Item), 'item')
+export const ComboboxItemGroup = withContext(styled(Ark.Combobox.ItemGroup), 'itemGroup')
+export const ComboboxItemGroupLabel = withContext(
+  styled(Ark.Combobox.ItemGroupLabel),
+  'itemGroupLabel',
 )
+export const ComboboxItemIndicator = withContext(
+  styled(Ark.Combobox.ItemIndicator),
+  'itemIndicator',
+)
+export const ComboboxItemText = withContext(styled(Ark.Combobox.ItemText), 'itemText')
+export const ComboboxLabel = withContext(styled(Ark.Combobox.Label), 'label')
 export const ComboboxPositioner = withContext(styled(Ark.Combobox.Positioner), 'positioner')
 export const ComboboxTrigger = withContext(styled(Ark.Combobox.Trigger), 'trigger')
 
@@ -29,10 +35,12 @@ export const Combobox = Object.assign(ComboboxRoot, {
   Content: ComboboxContent,
   Control: ComboboxControl,
   Input: ComboboxInput,
+  Item: ComboboxItem,
+  ItemGroup: ComboboxItemGroup,
+  ItemGroupLabel: ComboboxItemGroupLabel,
+  ItemIndicator: ComboboxItemIndicator,
+  ItemText: ComboboxItemText,
   Label: ComboboxLabel,
-  Option: ComboboxOption,
-  OptionGroup: ComboboxOptionGroup,
-  OptionGroupLabel: ComboboxOptionGroupLabel,
   Positioner: ComboboxPositioner,
   Trigger: ComboboxTrigger,
 })
