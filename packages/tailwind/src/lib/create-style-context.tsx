0e64773db4dc69558ef0dd585ada66997684b460
Simon Holzmayer <dev@holzmayer.me>
2023-11-14 21:17:18 +0100

    fix: strict variant and arc props in tailwind
    Closes Missing Arc types using Arc components with createStyleContext / withProvider #87

--- packages/tailwind/src/lib/create-style-context.tsx
+++ packages/tailwind/src/lib/create-style-context.tsx
@@ -2,14 +2,7 @@
 import { createContext, forwardRef, useContext, type ComponentProps, type ElementType } from 'react'
 
 type Recipe = (props: any) => any
-
 type VariantProps<R extends Recipe> = Parameters<R>[0]
-type Assign<T, U> = Omit<T, keyof U> & U
-
-export interface StyledContextProvider<T extends ElementType, R extends Recipe> {
-  (props: ComponentProps<T> & VariantProps<R>): JSX.Element
-  <P extends object>(props: Assign<ComponentProps<T>, P> & VariantProps<R>): JSX.Element
-}
 
 const cx = (...args: (string | undefined)[]) => args.filter(Boolean).join(' ')
 
@@ -24,39 +17,37 @@ export const createStyleContext = <
   const withProvider = <C extends ElementType>(Component: C, slot?: Slot) => {
     const Comp = forwardRef((props: ComponentProps<C> & VariantProps<StylesFunction>, ref) => {
       const styles = createStyles(props)
-      const localProps = props
-
       const variantClassNames = styles[slot ?? '']?.()
       return (
         <StyleContext.Provider value={styles}>
-          <Component
-            ref={ref}
-            {...localProps}
-            className={cx(variantClassNames, localProps.className)}
-          />
+          <Component ref={ref} {...props} className={cx(variantClassNames, props.className)} />
         </StyleContext.Provider>
       )
     })
     // @ts-expect-error JSX.IntrinsicElements do not have a displayName but Function and Class components do
     Comp.displayName = Component.displayName || Component.name || 'Component'
-    return Comp as unknown as StyledContextProvider<C, StylesFunction>
+    return Comp
   }
 
   const withContext = <C extends ElementType>(Component: C, slot?: Slot) => {
-    const Comp = forwardRef((props: ComponentProps<C>, ref) => {
-      const slotRecipe = useContext(StyleContext)
+    type ComponentPropsWithVariants = ComponentProps<C>
 
+    const Comp = forwardRef((props: ComponentPropsWithVariants, ref) => {
+      const slotRecipe = useContext(StyleContext)
       const variantClassNames = slotRecipe?.[slot ?? '']?.()
 
       return (
-        // @ts-expect-error
-        <Component ref={ref} {...props} className={cx(variantClassNames, props.className)} />
+        <Component
+          ref={ref}
+          {...(props as any)}
+          className={cx(variantClassNames, props.className)}
+        />
       )
     })
+
     // @ts-expect-error JSX.IntrinsicElements do not have a displayName but Function and Class components do
     Comp.displayName = Component.displayName || Component.name || 'Component'
-
-    return Comp as unknown as C
+    return Comp
   }
 
   return {
