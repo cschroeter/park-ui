4ea71524364a8bdb299d68f28d899ff91971e684
Christian Schröter <dev@grizzly.codes>
2023-11-17 13:05:05 +0100

    refactor(react): polish create-style-context

--- examples/panda/react/next-js/src/lib/create-style-context.tsx
+++ examples/panda/react/next-js/src/lib/create-style-context.tsx
@@ -8,52 +8,56 @@ import {
   type JSX,
 } from 'react'
 
-type Props = Record<string, unknown>
-type Recipe = {
-  (props?: Props): Record<string, string>
-  splitVariantProps: (props: Props) => any
+type GenericProps = Record<string, unknown>
+type StyleRecipe = {
+  (props?: GenericProps): Record<string, string>
+  splitVariantProps: (props: GenericProps) => any
 }
-type Slot<R extends Recipe> = keyof ReturnType<R>
-type SlotRecipe<R extends Recipe> = Record<Slot<R>, string>
-type VariantProps<R extends Recipe> = Parameters<R>[0]
-type Assign<T, U> = Omit<T, keyof U> & U
+type StyleSlot<R extends StyleRecipe> = keyof ReturnType<R>
+type StyleSlotRecipe<R extends StyleRecipe> = Record<StyleSlot<R>, string>
+type StyleVariantProps<R extends StyleRecipe> = Parameters<R>[0]
+type CombineProps<T, U> = Omit<T, keyof U> & U
 
 const cx = (...args: (string | undefined)[]) => args.filter(Boolean).join(' ')
 
-export interface StyledContextProvider<T extends ElementType, R extends Recipe> {
-  (props: Assign<ComponentProps<T>, VariantProps<R>>): JSX.Element
+export interface ComponentVariants<T extends ElementType, R extends StyleRecipe> {
+  (props: CombineProps<ComponentProps<T>, StyleVariantProps<R>>): JSX.Element
 }
 
-export const createStyleContext = <R extends Recipe>(recipe: R) => {
-  const SlotRecipeContext = createContext<SlotRecipe<R> | null>(null)
+export const createStyleContext = <R extends StyleRecipe>(recipe: R) => {
+  const StyleContext = createContext<StyleSlotRecipe<R> | null>(null)
 
-  const withProvider = <T extends ElementType>(Component: T, slot?: Slot<R>) => {
-    const Comp = forwardRef((props: ComponentProps<T>, ref) => {
-      const [variantProps, localProps] = recipe.splitVariantProps(props)
-      const slotRecipe = recipe(variantProps) as SlotRecipe<R>
+  const withProvider = <T extends ElementType>(
+    Component: T,
+    slot?: StyleSlot<R>,
+  ): ComponentVariants<T, R> => {
+    const StyledComponent = forwardRef((props: ComponentProps<T>, ref) => {
+      const [variantProps, otherProps] = recipe.splitVariantProps(props)
+      const slotStyles = recipe(variantProps) as StyleSlotRecipe<R>
       return (
-        <SlotRecipeContext.Provider value={slotRecipe}>
+        <StyleContext.Provider value={slotStyles}>
           <Component
             ref={ref}
-            {...localProps}
-            className={cx(slotRecipe[slot ?? ''], localProps.className)}
+            {...otherProps}
+            className={cx(slotStyles[slot ?? ''], otherProps.className)}
           />
-        </SlotRecipeContext.Provider>
+        </StyleContext.Provider>
       )
     })
-    return Comp as unknown as StyledContextProvider<T, R>
+    return StyledComponent as unknown as ComponentVariants<T, R>
   }
 
-  const withContext = <T extends ElementType>(Component: T, slot?: Slot<R>) => {
-    const Comp = forwardRef((props: ComponentProps<T>, ref) => {
-      const recipe = useContext(SlotRecipeContext)
+  const withContext = <T extends ElementType>(Component: T, slot?: StyleSlot<R>): T => {
+    if (!slot) return Component
+    const StyledComponent = forwardRef((props: ComponentProps<T>, ref) => {
+      const slotStyles = useContext(StyleContext)
       return createElement(Component, {
         ...props,
-        className: cx(recipe?.[slot ?? ''], props.className),
+        className: cx(slotStyles?.[slot ?? ''], props.className),
         ref,
       })
     })
-    return Comp as unknown as T
+    return StyledComponent as unknown as T
   }
 
   return {
