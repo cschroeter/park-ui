63d8611f086c6c1ceb3a268dfc8b236605ae6bc5
Christian Schröter <dev@grizzly.codes>
2023-11-24 14:16:50 +0100

    feat(cli): add json schema

--- packages/cli/src/config/config.ts
+++ packages/cli/src/config/config.ts
@@ -1,63 +1,8 @@
 import { readFileSync, writeFile } from 'fs'
-import { FromSchema } from 'json-schema-to-ts'
-// import jsonSchema from './schema.json'
 import { packageDirectory } from 'pkg-dir'
+import { ParkUiConfiguration } from '../../schema'
 
-const configSchema = {
-  $schema: 'http://json-schema.org/draft-07/schema#',
-  title: 'park-ui Configuration',
-  type: 'object',
-  properties: {
-    cssFramework: {
-      type: 'string',
-      enum: ['panda', 'tailwind'],
-      description: 'The CSS framework to use.',
-    },
-    jsFramework: {
-      type: 'string',
-      enum: ['React', 'Solid', 'Vue'],
-      description: 'The JavaScript framework to use.',
-    },
-    importAliases: {
-      type: 'object',
-      properties: {
-        components: {
-          type: 'string',
-          pattern: '^~/',
-          description: 'The import alias for components.',
-        },
-        utils: {
-          type: 'string',
-          pattern: '^~/',
-          description: 'The import alias for utilities.',
-        },
-      },
-      additionalProperties: false,
-      description: 'Configuration for import aliases.',
-    },
-  },
-  additionalProperties: false,
-  required: ['cssFramework', 'jsFramework', 'importAliases'],
-  if: {
-    properties: {
-      jsFramework: {
-        const: 'React',
-      },
-    },
-    required: ['jsFramework'],
-  },
-  then: {
-    properties: {
-      useReactServerComponents: {
-        type: 'boolean',
-        description: 'Whether React Server Components are being used.',
-      },
-    },
-    required: ['useReactServerComponents'],
-  },
-} as const // use asConst from json-schema-to-ts
-
-export type Config = FromSchema<typeof configSchema>
+export type Config = ParkUiConfiguration
 
 export const CONFIG_FILE_NAME = 'park-ui.json'
 
