53fa93b55acb95acb36ed7103cc18697da1576da
Christian Schröter <dev@grizzly.codes>
2023-11-25 14:20:25 +0100

    feat(scripts): improve components json cmd

--- packages/scripts/src/commands/components-json.ts
+++ packages/scripts/src/commands/components-json.ts
@@ -1,4 +1,5 @@
 import * as components from '@ark-ui/anatomy'
+import { AnatomyInstance } from '@ark-ui/anatomy'
 import { Command } from 'commander'
 import fs from 'fs-extra'
 import prettier from 'prettier'
@@ -7,48 +8,45 @@ import v from 'voca'
 
 const prettierConfig = await prettier.resolveConfig('.')
 
-const startsWithUpperCase = (str: string) => v.chain(str).first().isUpperCase().value()
 const pascalCase = (s: string) => v.chain(s).camelCase().capitalize().value()
 const camelCase = (s: string) => v.chain(s).camelCase().value()
 
-const getComponent = async (moduleName: string) =>
-  import(`@ark-ui/react/${moduleName}`).then((m) => m[pascalCase(moduleName)])
-
-const getAvailaeComponents = () =>
-  Object.keys(components)
-    .filter((item) => !['createAnatomy'].includes(item))
-    .map((key) => v.slugify(key.replace('Anatomy', '')))
-
 const generateComponentsJson = async () => {
-  const result = await Promise.all(
-    getAvailaeComponents().map(async (moduleName) => {
-      const component = await getComponent(moduleName)
+  const result = Object.entries(components)
+    .filter(([key]) => !['createAnatomy'].includes(key))
+    .map(([key, value]) => {
+      const component = v.slugify(key.replace('Anatomy', ''))
+
       return {
-        [moduleName]: {
-          components: Object.entries(component)
-            .filter(([key]) => startsWithUpperCase(key))
-            .reduce((acc, [key, value]) => {
-              return {
-                ...acc,
-                [key]: {
-                  name:
-                    // @ts-expect-error
-                    value.displayName ?? pascalCase(moduleName).concat(key === 'Root' ? '' : key),
-                  // Segment is an alias for Radio
-                  partName: camelCase(key.replace('Segment', 'Radio')),
-                },
-              }
-            }, {}),
+        [component]: {
+          components: (value as AnatomyInstance<string>)
+            .keys()
+            .sort((a, b) => {
+              if (a === 'root') return -1
+              if (b === 'root') return 1
+              return a.localeCompare(b)
+            })
+            .reduce(
+              (acc, part) => {
+                return {
+                  ...acc,
+                  [pascalCase(part)]: {
+                    name: pascalCase(component).concat(part === 'root' ? '' : pascalCase(part)),
+                    partName: part,
+                  },
+                }
+              },
+              { Root: { name: pascalCase(component) } },
+            ),
           isArkComponent: true,
-          rootComponent: pascalCase(moduleName),
-          filename: `${moduleName}.ts`,
-          className: match(moduleName)
+          rootComponent: pascalCase(component),
+          filename: `${component}.ts`,
+          className: match(component)
             .with('switch', () => 'switchRecipe') // resvered word
-            .otherwise(() => camelCase(moduleName)),
+            .otherwise(() => camelCase(component)),
         },
       }
-    }),
-  )
+    })
 
   const content = await prettier.format(
     JSON.stringify(result.reduce((acc, item) => ({ ...acc, ...item }))),
