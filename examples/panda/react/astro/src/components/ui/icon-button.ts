129eda36c0600b902e68d7b63c98e9e5c12d6e04
Christian Schröter <dev@grizzly.codes>
2023-12-15 09:35:00 +0100

    feat(examples): update astro example

--- examples/panda/react/astro/src/components/ui/icon-button.ts
+++
@@ -1,6 +0,0 @@
-import { ark } from '@ark-ui/react'
-import { styled, type HTMLStyledProps } from 'styled-system/jsx'
-import { iconButton } from 'styled-system/recipes'
-
-export const IconButton = styled(ark.button, iconButton)
-export interface IconButtonProps extends HTMLStyledProps<typeof IconButton> {}
