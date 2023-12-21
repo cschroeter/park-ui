f9c05cddc2a3c952b10e49345ba5a9391bf58b5d
Christian Schröter <dev@grizzly.codes>
2023-12-21 17:00:18 +0100

    feat(chakra): only add multi part components

--- scripts/src/commands/registry.ts
+++ scripts/src/commands/registry.ts
@@ -40,6 +40,13 @@ const generateIndex = async (options: Options) => {
     JSON.stringify({
       components: Object.entries(data)
         .sort(([a], [b]) => a.localeCompare(b))
+        // if the css framework is chakra, we only want to show components that are not using the parts api
+        .filter(([_, value]) => {
+          if (cssFramwork === 'chakra') {
+            return value.hasOwnProperty('parts')
+          }
+          return true
+        })
         .map(([componentName, value]) => ({
           name: pascalCase(value.name),
           href: `https://park-ui.com/registry/${cssFramwork}/${jsFramework}/components/${componentName}.json`,
@@ -71,64 +78,71 @@ const generateComponents = async (options: Options) => {
 
   const { cssFramwork, jsFramework } = options
   await Promise.all(
-    Object.entries(data).map(async ([key, value]) => {
-      const view = {
-        key,
-        ...value,
-        imports: value.imports[jsFramework],
-        tvConfig: JSON.stringify(transformComponentToTvConfig(value.className)),
-      }
-
-      const variant = value.hasOwnProperty('parts') ? 'with-context' : 'without-context'
-
-      const template = Handlebars.compile(
-        fs.readFileSync(`./src/templates/${cssFramwork}/${jsFramework}/${variant}.hbs`, 'utf-8'),
-      )
-
-      const templateString = template(view)
-
-      const code = await prettier.format(templateString, {
-        ...prettierConfig,
-        plugins: ['prettier-plugin-organize-imports'],
-        parser: 'typescript',
+    Object.entries(data)
+      .filter(([_, value]) => {
+        if (cssFramwork === 'chakra') {
+          return value.hasOwnProperty('parts')
+        }
+        return true
       })
+      .map(async ([key, value]) => {
+        const view = {
+          key,
+          ...value,
+          imports: value.imports[jsFramework],
+          tvConfig: JSON.stringify(transformComponentToTvConfig(value.className)),
+        }
 
-      const content = await prettier.format(
-        JSON.stringify({
-          files: [
-            {
-              filename: `${key}.ts`,
-              content: code,
-              hasMultipleParts: value.hasOwnProperty('parts'),
-            },
-          ],
-        }),
-        {
+        const variant = value.hasOwnProperty('parts') ? 'with-context' : 'without-context'
+
+        const template = Handlebars.compile(
+          fs.readFileSync(`./src/templates/${cssFramwork}/${jsFramework}/${variant}.hbs`, 'utf-8'),
+        )
+
+        const templateString = template(view)
+
+        const code = await prettier.format(templateString, {
           ...prettierConfig,
-          parser: 'json',
-        },
-      )
+          plugins: ['prettier-plugin-organize-imports'],
+          parser: 'typescript',
+        })
 
-      // await fs.outputFile(path.join(`dist/${cssFramwork}/${jsFramework}/${key}.tsx`), code)
-      // await fs.outputFile(
-      //   path.join(rootDir, 'website', 'src', 'components', 'ui', `${key}.tsx`),
-      //   code,
-      // )
+        const content = await prettier.format(
+          JSON.stringify({
+            files: [
+              {
+                filename: `${key}.ts`,
+                content: code,
+                hasMultipleParts: value.hasOwnProperty('parts'),
+              },
+            ],
+          }),
+          {
+            ...prettierConfig,
+            parser: 'json',
+          },
+        )
 
-      await fs.outputFile(
-        path.join(
-          rootDir,
-          'website',
-          'public',
-          'registry',
-          cssFramwork,
-          jsFramework,
-          'components',
-          key + '.json',
-        ),
-        content,
-      )
-    }),
+        // await fs.outputFile(path.join(`dist/${cssFramwork}/${jsFramework}/${key}.tsx`), code)
+        // await fs.outputFile(
+        //   path.join(rootDir, 'website', 'src', 'components', 'ui', `${key}.tsx`),
+        //   code,
+        // )
+
+        await fs.outputFile(
+          path.join(
+            rootDir,
+            'website',
+            'public',
+            'registry',
+            cssFramwork,
+            jsFramework,
+            'components',
+            key + '.json',
+          ),
+          content,
+        )
+      }),
   )
 }
 
