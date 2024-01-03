23f7289d19352500140e7e3b77123dc2c046f9f3
kovsu <2583695112@qq.com>
2024-01-03 12:00:49 +0800

    feat(rating-group): revise rating group component api

---
+++ components/react/src/rating-group/rating-group.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta } from '@storybook/react'
+import { RatingGroup, type RatingGroupProps } from './rating-group'
+
+const meta: Meta<RatingGroupProps> = {
+  title: 'RatingGroup',
+  component: RatingGroup,
+}
+
+export default meta
+
+export const Base = () => <RatingGroup>Label</RatingGroup>
+export const ColorPalette = () => <RatingGroup colorPalette="red">Label</RatingGroup>
+export const DefaultValue = () => <RatingGroup defaultValue={3}>Label</RatingGroup>
+export const MoreCount = () => <RatingGroup count={10}>Label</RatingGroup>
---
+++ components/solid/src/rating-group/rating-group.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta } from 'storybook-solidjs'
+import { RatingGroup, type RatingGroupProps } from './rating-group'
+
+const meta: Meta<RatingGroupProps> = {
+  title: 'RatingGroup',
+  component: RatingGroup,
+}
+
+export default meta
+
+export const Base = () => <RatingGroup>Label</RatingGroup>
+export const ColorPalette = () => <RatingGroup colorPalette="red">Label</RatingGroup>
+export const DefaultValue = () => <RatingGroup value={3}>Label</RatingGroup>
+export const MoreCount = () => <RatingGroup count={10}>Label</RatingGroup>
