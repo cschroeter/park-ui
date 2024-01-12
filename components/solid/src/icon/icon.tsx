2c2f1db6cad53fecd439ee02de985ac6a08b2f83
Christian Schröter <dev@grizzly.codes>
2024-01-12 11:13:35 +0100

    feat(components): document icon

---
+++ components/solid/src/icon/icon.tsx
@@ -0,0 +1,14 @@
+import { ark } from '@ark-ui/solid'
+import { type JSX } from 'solid-js'
+import { styled, type HTMLStyledProps } from 'styled-system/jsx'
+import { icon, type IconVariantProps } from 'styled-system/recipes'
+
+export interface IconProps extends IconVariantProps, HTMLStyledProps<'svg'> {
+  children: JSX.Element
+}
+
+export const Icon = (props: IconProps) => {
+  return <StyledIcon asChild {...props} />
+}
+
+const StyledIcon = styled(ark.svg, icon)
