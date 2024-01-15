e8b8bdf2ab34a226027a93f1960e3ed776d951b6
kovsu <2583695112@qq.com>
2024-01-15 18:07:22 +0800

    chore: update demo

--- website/src/components/demos/skeleton.demo.tsx
+++ website/src/components/demos/skeleton.demo.tsx
@@ -1,23 +1,15 @@
-import { useState } from 'react'
+import { HStack, Stack } from 'styled-system/jsx'
 import { Skeleton, type SkeletonProps } from '~/components/ui/skeleton'
-import { Switch } from '~/components/ui/switch'
 
 export const Demo = (props: SkeletonProps) => {
-  const [checked, setChecked] = useState(true)
   return (
-    <div>
-      <Switch checked={checked} onCheckedChange={(e) => setChecked(e.checked)} />
-      <Skeleton loading={checked} {...props} width={'100px'} height={'100px'}>
-        <img
-          src="https://avatars.githubusercontent.com/u/82451257?v=4"
-          alt="Konv Suu"
-          style={{
-            width: '100%',
-            height: '100%',
-            borderRadius: '50%',
-          }}
-        />
-      </Skeleton>
-    </div>
+    <HStack>
+      <Skeleton {...props} width={'64px'} height={'64px'} />
+      <Stack>
+        <Skeleton {...props} width={'200px'} height={'12px'} />
+        <Skeleton {...props} width={'180px'} height={'12px'} />
+        <Skeleton {...props} width={'120px'} height={'12px'} />
+      </Stack>
+    </HStack>
   )
 }
