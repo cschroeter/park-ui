5ce444fdec0667bb965fab8b912e56e724f5dac4
Christian Schröter <dev@grizzly.codes>
2024-01-19 11:11:14 +0100

    chore: poc for vue components

---
+++ components/panda/vue/src/stories/avatar.story.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Avatar from '../components/ui/Avatar.vue'
+</script>
+
+<template>
+  <Story title="Test">
+    <Variant title="Basic">
+      <Avatar name="Christian Schröter" src="https://avatars.githubusercontent.com/u/1846056?v=4" />
+    </Variant>
+  </Story>
+</template>
--- components/panda/vue/src/stories/test.story.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <Story title="Test">
-    <Variant title="Basic">
-      <h1>Hello</h1>
-    </Variant>
-  </Story>
-</template>
