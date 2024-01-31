79d635b28607fb950e88f99d59f8cb5dc5b5da61
Christian Schröter <dev@grizzly.codes>
2024-01-31 22:50:54 +0100

    chore: wip

--- components/tailwind/solid/src/components/ui/text.tsx
+++ components/tailwind/solid/src/components/ui/text.tsx
@@ -1,15 +1,42 @@
 import { mergeProps, splitProps, type JSX } from 'solid-js'
 import { Dynamic } from 'solid-js/web'
+import { tv, type VariantProps } from 'tailwind-variants'
 
 type As = 'p' | 'span' | 'div' | 'label'
 
 export type TextProps = {
   as?: As
-} & JSX.IntrinsicElements[As]
+} & TextVariantProps &
+  JSX.IntrinsicElements[As]
 
 export const Text = (props: TextProps) => {
   const mergedProps = mergeProps({ as: 'p' }, props)
-  const [localProps, rootProps] = splitProps(mergedProps, ['as'])
+  const [variantProps, textProps] = splitProps(mergedProps, ['size', 'class'])
+  const [localProps, rootProps] = splitProps(textProps, ['as'])
+  // @ts-expect-error https://github.com/nextui-org/tailwind-variants/issues/145
+  const className = styles(variantProps)
 
-  return <Dynamic component={localProps.as} {...rootProps} />
+  return <Dynamic component={localProps.as} class={className} {...rootProps} />
 }
+
+type TextVariantProps = VariantProps<typeof styles>
+
+const styles = tv({
+  base: 'text',
+  variants: {
+    variant: { heading: 'text--variant_heading' },
+    size: {
+      xs: 'text--size_xs',
+      sm: 'text--size_sm',
+      md: 'text--size_md',
+      lg: 'text--size_lg',
+      xl: 'text--size_xl',
+      '2xl': 'text--size_2xl',
+      '3xl': 'text--size_3xl',
+      '4xl': 'text--size_4xl',
+      '5xl': 'text--size_5xl',
+      '6xl': 'text--size_6xl',
+      '7xl': 'text--size_7xl',
+    },
+  },
+})
