94305353f42c4bfa3d1d7c0a8920a103f83b6055
Christian Schröter <dev@grizzly.codes>
2024-02-02 12:24:26 +0100

    chore: bump deps

--- components/panda/react/src/components/ui/text.tsx
+++ components/panda/react/src/components/ui/text.tsx
@@ -2,7 +2,7 @@ import { useMemo } from 'react'
 import { styled, type HTMLStyledProps, type StyledComponent } from 'styled-system/jsx'
 import { text, type TextVariantProps } from 'styled-system/recipes'
 
-type As = 'p' | 'span' | 'label' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+type As = 'p' | 'span' | 'div' | 'label' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 
 export type TextProps = {
   as?: As
