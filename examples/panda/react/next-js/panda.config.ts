1ba917d441a8984645fea5749f718f1224094f1b
Christian Schröter <dev@grizzly.codes>
2024-02-12 10:38:31 +0100

    feat(examples): update panda nextjs example

--- examples/panda/react/next-js/panda.config.ts
+++ examples/panda/react/next-js/panda.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from '@pandacss/dev'
+import { createPreset } from '@park-ui/panda-preset'
 
 export default defineConfig({
   preflight: true,
-  presets: ['@pandacss/preset-base', '@park-ui/panda-preset'],
+  presets: [
+    '@pandacss/preset-base',
+    createPreset({
+      accentColor: 'amber',
+      grayColor: 'sand',
+    }),
+  ],
   include: ['./src/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
   exclude: [],
   theme: {
