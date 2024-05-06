b809d7a11ec96f312fafef671f5b16f19debe4ce
Christian Schröter <dev@grizzly.codes>
2024-05-06 09:27:02 +0200

    feat(panda): optimized type checking for typography components

--- components/panda/react/src/components/ui/heading.tsx
+++ components/panda/react/src/components/ui/heading.tsx
@@ -1,19 +1,10 @@
-import { forwardRef, useMemo } from 'react'
-import { type HTMLStyledProps, type StyledComponent, styled } from 'styled-system/jsx'
-import { type TextVariantProps, text } from 'styled-system/recipes'
+import { forwardRef } from 'react'
+import { Text, type TextProps } from './text'
 
-type As = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+interface HeadingProps extends TextProps<React.ElementType> {}
 
-export type HeadingProps = {
-  as?: As
-} & TextVariantProps &
-  HTMLStyledProps<As>
-
-export const Heading = forwardRef<HTMLHeadingElement, HeadingProps>((props, ref) => {
-  const { as = 'h2', ...localProps } = props
-  const Dynamic = useMemo(() => styled(as, text) as StyledComponent<As>, [as])
-
-  return <Dynamic ref={ref} {...localProps} />
-})
+export const Heading = forwardRef<HTMLHeadingElement, HeadingProps>((props, ref) => (
+  <Text as="h2" variant="heading" ref={ref} {...props} />
+))
 
 Heading.displayName = 'Heading'
--- website/src/components/ui/text.tsx
+++ website/src/components/ui/text.tsx
@@ -1,17 +1,39 @@
-import { useMemo } from 'react'
-import { styled, type HTMLStyledProps, type StyledComponent } from 'styled-system/jsx'
-import { text, type TextVariantProps } from 'styled-system/recipes'
+import React from 'react'
+import type { TextVariantProps } from 'styled-system/recipes'
+import type { HTMLStyledProps } from 'styled-system/types'
 
-type As = 'p' | 'span' | 'div' | 'label'
+type PolymorphicRef<C extends React.ElementType> = React.ComponentPropsWithRef<C>['ref']
 
-export type TextProps = {
-  as?: As
-} & TextVariantProps &
-  HTMLStyledProps<As>
-
-export const Text = (props: TextProps) => {
-  const { as = 'p', ...localProps } = props
-  const Dynamic = useMemo(() => styled(as, text) as StyledComponent<As>, [as])
-
-  return <Dynamic {...localProps} />
+type AsProp<C extends React.ElementType> = {
+  as?: C
 }
+
+type PropsToOmit<C extends React.ElementType, P> = keyof (AsProp<C> & P)
+
+type PolymorphicComponentProp<C extends React.ElementType, Props = {}> = React.PropsWithChildren<
+  Props & AsProp<C>
+> &
+  Omit<React.ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>
+
+type PolymorphicComponentPropWithRef<
+  C extends React.ElementType,
+  Props = {},
+> = PolymorphicComponentProp<C, Props> & { ref?: PolymorphicRef<C> }
+
+export type TextProps<C extends React.ElementType> = PolymorphicComponentPropWithRef<
+  C,
+  HTMLStyledProps<C> & TextVariantProps
+>
+
+type PolymorphicComponent = <C extends React.ElementType = 'p'>(
+  props: TextProps<C>,
+) => React.ReactNode | null
+
+export const Text: PolymorphicComponent = React.forwardRef(
+  <C extends React.ElementType = 'p'>(props: TextProps<C>, ref?: PolymorphicRef<C>) => {
+    const { as, ...textProps } = props
+    const Component = as || 'p'
+
+    return <Component ref={ref} {...textProps} />
+  },
+)
