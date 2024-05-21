c92296ad3f9b9622406edec0e235483ad484b76f
Christian Schröter <dev@grizzly.codes>
2024-05-21 16:49:19 +0200

    feat(react): improve create-style-context fn

--- components/panda/react/src/components/ui/accordion.tsx
+++ components/panda/react/src/components/ui/accordion.tsx
@@ -1,19 +1,42 @@
 import { Accordion } from '@ark-ui/react/accordion'
-import type { ComponentProps } from 'react'
-import { styled } from 'styled-system/jsx'
-import { accordion } from 'styled-system/recipes'
+import { type AccordionVariantProps, accordion } from 'styled-system/recipes'
+import type { Assign, JsxStyleProps } from 'styled-system/types'
 import { createStyleContext } from '~/lib/create-style-context'
 
 const { withProvider, withContext } = createStyleContext(accordion)
 
-export const Root = withProvider(styled(Accordion.Root), 'root')
-export const Item = withContext(styled(Accordion.Item), 'item')
-export const ItemContent = withContext(styled(Accordion.ItemContent), 'itemContent')
-export const ItemIndicator = withContext(styled(Accordion.ItemIndicator), 'itemIndicator')
-export const ItemTrigger = withContext(styled(Accordion.ItemTrigger), 'itemTrigger')
+export interface RootProps
+  extends Assign<JsxStyleProps, Accordion.RootProps>,
+    AccordionVariantProps {}
+export const Root = withProvider<HTMLDivElement, RootProps>(Accordion.Root, 'root')
 
-export interface RootProps extends ComponentProps<typeof Root> {}
-export interface ItemProps extends ComponentProps<typeof Item> {}
-export interface ItemContentProps extends ComponentProps<typeof ItemContent> {}
-export interface ItemIndicatorProps extends ComponentProps<typeof ItemIndicator> {}
-export interface ItemTriggerProps extends ComponentProps<typeof ItemTrigger> {}
+export interface ItemProps extends Assign<JsxStyleProps, Accordion.ItemProps> {}
+export const Item = withContext<HTMLDivElement, ItemProps>(Accordion.Item, 'item')
+
+export interface ItemTriggerProps extends Assign<JsxStyleProps, Accordion.ItemTriggerProps> {}
+export const ItemTrigger = withContext<HTMLDivElement, ItemTriggerProps>(
+  Accordion.ItemTrigger,
+  'itemTrigger',
+)
+
+export interface ItemIndicatorProps extends Assign<JsxStyleProps, Accordion.ItemIndicatorProps> {}
+export const ItemIndicator = withContext<HTMLButtonElement, ItemIndicatorProps>(
+  Accordion.ItemIndicator,
+  'itemIndicator',
+)
+
+export interface ItemContentProps extends Assign<JsxStyleProps, Accordion.ItemContentProps> {}
+export const ItemContent = withContext<HTMLDivElement, ItemContentProps>(
+  Accordion.ItemContent,
+  'itemContent',
+)
+
+export {
+  AccordionContext as Context,
+  type AccordionContextProps as ContextProps,
+} from '@ark-ui/react/accordion'
+
+export type {
+  AccordionFocusChangeDetails as FocusChangeDetails,
+  AccordionValueChangeDetails as ValueChangeDetails,
+} from '@ark-ui/react/accordion'
--- components/panda/react/src/components/ui/popover.tsx
+++ components/panda/react/src/components/ui/popover.tsx
@@ -1,12 +1,13 @@
 import { Popover } from '@ark-ui/react/popover'
-import type { ComponentProps } from 'react'
 import { styled } from 'styled-system/jsx'
-import { popover } from 'styled-system/recipes'
+import { type PopoverVariantProps, popover } from 'styled-system/recipes'
 import { createStyleContext } from '~/lib/create-style-context'
 
-const { withProvider, withContext } = createStyleContext(popover)
+const { withRootProvider, withContext } = createStyleContext(popover)
 
-export const Root = withProvider(Popover.Root)
+export interface RootProps extends Popover.RootProps, PopoverVariantProps {}
+
+export const Root = withRootProvider<RootProps>(Popover.Root)
 export const Anchor = withContext(styled(Popover.Anchor), 'anchor')
 export const Arrow = withContext(styled(Popover.Arrow), 'arrow')
 export const ArrowTip = withContext(styled(Popover.ArrowTip), 'arrowTip')
@@ -17,15 +18,3 @@ export const Indicator = withContext(styled(Popover.Indicator), 'indicator')
 export const Positioner = withContext(styled(Popover.Positioner), 'positioner')
 export const Title = withContext(styled(Popover.Title), 'title')
 export const Trigger = withContext(styled(Popover.Trigger), 'trigger')
-
-export interface RootProps extends ComponentProps<typeof Root> {}
-export interface AnchorProps extends ComponentProps<typeof Anchor> {}
-export interface ArrowProps extends ComponentProps<typeof Arrow> {}
-export interface ArrowTipProps extends ComponentProps<typeof ArrowTip> {}
-export interface CloseTriggerProps extends ComponentProps<typeof CloseTrigger> {}
-export interface ContentProps extends ComponentProps<typeof Content> {}
-export interface DescriptionProps extends ComponentProps<typeof Description> {}
-export interface IndicatorProps extends ComponentProps<typeof Indicator> {}
-export interface PositionerProps extends ComponentProps<typeof Positioner> {}
-export interface TitleProps extends ComponentProps<typeof Title> {}
-export interface TriggerProps extends ComponentProps<typeof Trigger> {}
