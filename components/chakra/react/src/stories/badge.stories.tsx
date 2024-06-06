4680d648201daf3b3f2fb7744a3abb296809e018
Christian Schröter <dev@grizzly.codes>
2024-06-06 21:19:21 +0200

    feat(chakra): drop support for chakra as this will be baked in chakra v3

--- components/chakra/react/src/stories/badge.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Badge } from '@chakra-ui/react'
-import type { Meta } from '@storybook/react'
-
-const meta: Meta = {
-  title: 'Components/Badge',
-}
-export default meta
-
-export const Default = () => <Badge>Badge</Badge>
