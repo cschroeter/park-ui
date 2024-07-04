a8932f537a0fa25da58149ab8d994e7b2f30e7a5
Christian Schröter <dev@grizzly.codes>
2024-07-04 20:55:56 +0200

    chore: housekeeping

--- components/vue/vite.config.ts
+++ components/vue/vite.config.ts
@@ -1,7 +1,8 @@
 import Vue from '@vitejs/plugin-vue'
 import VueJsx from '@vitejs/plugin-vue-jsx'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [Vue(), VueJsx()],
+  plugins: [Vue(), VueJsx(), tsconfigPaths({ root: './' })],
 })
