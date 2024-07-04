62a6b6df61544dd21a34f915546735dd7e5b982b
Christian Schröter <dev@grizzly.codes>
2024-07-04 20:47:41 +0200

    feat: drop support for tailwind

--- examples/tailwind/solid/vite/src/index.tsx
+++
@@ -1,8 +0,0 @@
-/* @refresh reload */
-import { render } from 'solid-js/web'
-import { App } from './App'
-import './index.css'
-
-const root = document.getElementById('root')
-
-render(() => <App />, root!)
