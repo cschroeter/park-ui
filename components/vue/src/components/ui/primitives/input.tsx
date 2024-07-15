261a928f541109a494ceab91e0ed0c528bc2a5e4
Christian Schröter <dev@grizzly.codes>
2024-07-15 19:32:30 +0200

    feat(vue): add input and card (#376)

---
+++ components/vue/src/components/ui/primitives/input.tsx
@@ -0,0 +1,6 @@
+import { styled } from 'styled-system/jsx'
+import { input } from 'styled-system/recipes'
+import type { ComponentProps } from 'styled-system/types'
+
+export type InputProps = ComponentProps<typeof Input>
+export const Input = styled('input', input)
