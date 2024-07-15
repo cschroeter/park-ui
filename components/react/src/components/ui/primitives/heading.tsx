ec2b79ab7ce11d4d209e66c4afc6c48d2fdbbd7e
Christian Schröter <dev@grizzly.codes>
2024-07-15 20:47:40 +0200

    feat(react): simplfy typography

--- components/react/src/components/ui/primitives/heading.tsx
+++ components/react/src/components/ui/primitives/heading.tsx
@@ -1,15 +1,10 @@
-import type { Assign } from '@ark-ui/react'
-import { type ElementType, forwardRef } from 'react'
-import { type HTMLStyledProps, styled } from 'styled-system/jsx'
+import { styled } from 'styled-system/jsx'
 import { type TextVariantProps, text } from 'styled-system/recipes'
-import type { ComponentProps } from 'styled-system/types'
+import type { ComponentProps, StyledComponent } from 'styled-system/types'
 
-const StyledHeading = styled('h2', text)
+type TextProps = TextVariantProps & { as?: React.ElementType }
 
-export type Headingprops = ComponentProps<typeof Heading>
-export const Heading = forwardRef<
-  HTMLHeadingElement,
-  Assign<Assign<HTMLStyledProps<'h2'>, TextVariantProps>, { as?: ElementType }>
->((props, ref) => <StyledHeading ref={ref} variant="heading" {...props} />)
-
-Heading.displayName = 'Heading'
+export type HeadingProps = ComponentProps<typeof Heading>
+export const Heading = styled('h2', text, {
+  defaultProps: { variant: 'heading' },
+}) as StyledComponent<'h2', TextProps>
