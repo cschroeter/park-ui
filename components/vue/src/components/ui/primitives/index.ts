1d22bae2142d969ec7e8191043235e6f620e8187
Christian Schröter <dev@grizzly.codes>
2024-07-15 07:32:03 +0200

    feat(vue): add card

--- components/vue/src/components/ui/primitives/index.ts
+++ components/vue/src/components/ui/primitives/index.ts
@@ -1,1 +1,2 @@
 export { Button, type ButtonProps } from './button'
+export * as Card from './card'
---
+++ components/vue/src/stories/card.story.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Card } from '~/components/ui'
+</script>
+
+<template>
+  <Story title="Card">
+    <Variant title="Basic">
+      <Card.Root maxW="sm">
+        <Card.Header>
+          <Card.Title>Team Members</Card.Title>
+          <Card.Description>Add new members to your organisation.</Card.Description>
+        </Card.Header>
+        <Card.Body>
+        
+        </Card.Body>
+        <Card.Footer>Footer</Card.Footer>
+      </Card.Root>
+    </Variant>
+  </Story>
+</template>
