dff6ecec88deaf17c4f58abbe40b60a1d6e05534
Christian Schröter <dev@grizzly.codes>
2024-07-15 21:57:27 +0200

    feat(react): simplify icon

--- components/react/src/components/ui/primitives/icon.tsx
+++ components/react/src/components/ui/primitives/icon.tsx
@@ -1,11 +1,9 @@
 import { ark } from '@ark-ui/react/factory'
-import { type ComponentProps, forwardRef } from 'react'
 import { styled } from 'styled-system/jsx'
 import { icon } from 'styled-system/recipes'
+import type { ComponentProps } from 'styled-system/types'
 
-export type IconProps = ComponentProps<typeof StyledIcon>
-const StyledIcon = styled(ark.svg, icon)
-
-export const Icon = forwardRef<SVGSVGElement, IconProps>((props, ref) => (
-  <StyledIcon asChild ref={ref} {...props} />
-))
+export type IconProps = ComponentProps<typeof Icon>
+export const Icon = styled(ark.svg, icon, {
+  defaultProps: { asChild: true },
+})
