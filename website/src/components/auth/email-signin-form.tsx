6cde6f5559f7b4369df8e81a34790f56d2c904ae
Christian Schröter <dev@grizzly.codes>
2024-07-23 16:53:52 +0200

    feat(website): add auth

---
+++ website/src/components/auth/email-signin-form.tsx
@@ -0,0 +1,29 @@
+import { Stack } from 'styled-system/jsx'
+import { signInWithEmail } from '~/app/actions'
+import { Button, Input } from '~/components/ui'
+
+interface Props {
+  redirectTo: string
+}
+
+export const EmailSignInForm = (props: Props) => {
+  const { redirectTo } = props
+
+  return (
+    <form action={signInWithEmail}>
+      <input type="hidden" name="redirectTo" value={redirectTo} />
+      <Stack gap="3">
+        <Input
+          name="email"
+          type="email"
+          required
+          placeholder="Enter your email"
+          autoComplete="off"
+        />
+        <Button type="submit" width="full">
+          Continue with Email
+        </Button>
+      </Stack>
+    </form>
+  )
+}
