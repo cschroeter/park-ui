e0f4d2ee189bc5d3c66a33f7286c64790774fa0a
Christian Schröter <dev@grizzly.codes>
2024-07-28 21:08:15 +0200

    feat(website): document invalid state

--- packages/panda/src/theme/recipes/field.ts
+++ packages/panda/src/theme/recipes/field.ts
@@ -4,5 +4,25 @@ import { defineSlotRecipe } from '@pandacss/dev'
 export const field = defineSlotRecipe({
   className: 'field',
   slots: fieldAnatomy.keys(),
-  base: {},
+  base: {
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '1.5',
+    },
+    label: {
+      color: 'fg.default',
+      fontWeight: 'medium',
+      textStyle: 'sm',
+    },
+    helperText: {
+      color: 'fg.muted',
+      textStyle: 'sm',
+    },
+    errorText: {
+      color: 'fg.error',
+      fontWeight: 'medium',
+      textStyle: 'sm',
+    },
+  },
 })
