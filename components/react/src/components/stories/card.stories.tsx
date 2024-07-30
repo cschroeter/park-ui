4a980c2cfcad8ae52607c4beb466567e84d0e1be
Christian Schröter <dev@grizzly.codes>
2024-07-30 02:02:24 +0200

    refactor: use field in card

--- components/react/src/components/stories/card.stories.tsx
+++ components/react/src/components/stories/card.stories.tsx
@@ -1,5 +1,9 @@
 import type { Meta } from '@storybook/react'
+import { Stack } from 'styled-system/jsx'
+import { Button } from '~/components/ui/button'
 import { Card } from '~/components/ui/card'
+import { Field } from '~/components/ui/field'
+import { Input } from '~/components/ui/input'
 
 const meta: Meta = {
   title: 'Components/Card',
@@ -14,8 +18,26 @@ export const Base = () => {
         <Card.Title>Team Members</Card.Title>
         <Card.Description>Add new members to your organisation.</Card.Description>
       </Card.Header>
-      <Card.Body>Body</Card.Body>
-      <Card.Footer>Footer</Card.Footer>
+      <Card.Body>
+        <Stack gap="4">
+          <Field.Root>
+            <Field.Label>Name</Field.Label>
+            <Field.Input asChild>
+              <Input placeholder="Name" />
+            </Field.Input>
+          </Field.Root>
+          <Field.Root>
+            <Field.Label>Email</Field.Label>
+            <Field.Input asChild>
+              <Input type="email" placeholder="Email" />
+            </Field.Input>
+          </Field.Root>
+        </Stack>
+      </Card.Body>
+      <Card.Footer gap="3">
+        <Button variant="outline">Cancel</Button>
+        <Button>Invite</Button>
+      </Card.Footer>
     </Card.Root>
   )
 }
