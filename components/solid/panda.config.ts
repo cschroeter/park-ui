c3999ac0b1dcc25bd5e93cb9fe859a0b9c10f111
Christian Schröter <dev@grizzly.codes>
2024-11-15 17:55:33 +0100

    fix build

--- components/solid/panda.config.ts
+++ components/solid/panda.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from '@pandacss/dev'
 import { createPreset } from '@park-ui/panda-preset'
+import amber from '@park-ui/panda-preset/colors/amber'
+import sand from '@park-ui/panda-preset/colors/sand'
 
 export default defineConfig({
   preflight: true,
-  presets: ['@pandacss/preset-base', createPreset({ additionalColors: ['red', 'green', 'blue'] })],
-  include: ['./src/**/*.{js,jsx,ts,tsx}'],
+  presets: [createPreset({ accentColor: amber, grayColor: sand, radius: 'sm' })],
+  include: ['./src/**/*.{js,jsx,ts,tsx,vue}'],
   jsxFramework: 'solid',
   outdir: 'styled-system',
-  staticCss: {
-    recipes: {
-      input: ['*'],
-    },
-  },
 })
