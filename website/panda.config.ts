ff241aef9b9e90c480c3c2e980ee211147554513
Christian Schröter <dev@grizzly.codes>
2025-04-26 16:14:37 +0200

    bump ark and react

--- website/panda.config.ts
+++ website/panda.config.ts
@@ -46,11 +46,12 @@ export default defineConfig({
       },
     }),
   ],
+  importMap: 'styled-system',
+  outdir: '../packages/styled-system/dist',
   preflight: true,
   jsxFramework: 'react',
   include: ['./src/components/**/*.{ts,tsx,js,jsx}', './src/app/**/*.{ts,tsx,js,jsx}'],
   exclude: [],
-  outdir: 'styled-system',
   patterns: {
     extend: {
       container: {
@@ -211,43 +212,6 @@ export default defineConfig({
           l1: { value: '{radii.xs}' },
           l2: { value: '{radii.sm}' },
           l3: { value: '{radii.md}' },
-        },
-      },
-      slotRecipes: {
-        layout: {
-          className: 'layout',
-          slots: ['aside', 'main'],
-          base: {
-            aside: {
-              bg: {
-                base: 'gray.2',
-                _dark: '#0e0e0e',
-              },
-              borderRightWidth: '1px',
-              position: 'fixed',
-              top: '0',
-              bottom: '0',
-              display: { base: 'none', md: 'block' },
-              ps: 'max(32px, calc((100vw - (1440px - 64px)) / 2))',
-              pe: '8',
-              minWidth: '272px',
-              overflow: 'auto',
-              width: {
-                base: '272px',
-                lg: 'calc((100vw - (1440px - 64px)) / 2 + 272px - 32px)',
-              },
-              zIndex: 'overlay',
-            },
-            main: {
-              minWidth: '0',
-              flex: '1',
-              ps: {
-                base: '0',
-                md: 'max(calc((100vw - 1440px) / 2 + 272px), 272px)',
-              },
-              pe: 'calc((100vw - 1440px) / 2)',
-            },
-          },
         },
       },
     },
