3564f74ae35cd027c7a2844952ab492239ee0dbb
Christian Busch <dev@grizzly.codes>
2025-08-20 10:00:43 +0200

    update colors in registry

--- registry.ts
+++ registry.ts
@@ -135,7 +135,7 @@ const generateColors = async () => {
                 extend: {
                   semanticTokens: {
                     colors: {
-                      [key]: id,
+                      [key]: `{${id}}`,
                     },
                   },
                 },
