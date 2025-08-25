836c31b8f737a296ff9a1a8026f673bd229180ff
Christian Schröter <dev@grizzly.codes>
2025-08-25 15:29:33 +0200

    rm old theme

--- packages/preset/src/__old/semantic-tokens/index.ts
+++
@@ -1,8 +0,0 @@
-import { defineSemanticTokens } from '@pandacss/dev'
-import { colors } from './colors'
-import { shadows } from './shadows'
-
-export const semanticTokens = defineSemanticTokens({
-  colors,
-  shadows,
-})
--- packages/preset/src/__old/semantic-tokens/radii.ts
+++
@@ -1,7 +0,0 @@
-import { defineSemanticTokens } from '@pandacss/dev'
-
-export const radii = defineSemanticTokens.radii({
-  l1: { value: '{radii.xs}' },
-  l2: { value: '{radii.sm}' },
-  l3: { value: '{radii.md}' },
-})
