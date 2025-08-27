9e75067140535aea286d5022c8ef757171f05242
Christian Schröter <dev@grizzly.codes>
2025-08-27 15:56:36 +0200

    replace demos with standalone examples

--- website/src/components/docs/quickstart-guide.tsx
+++ website/src/components/docs/quickstart-guide.tsx
@@ -1,20 +1,20 @@
 import { cva } from 'styled-system/css'
 import { Box, Wrap } from 'styled-system/jsx'
 import { Text } from '@/components/ui'
-import { NextJsIcon, NuxtIcon, SolidStartIcon } from '../icons'
+import { NextJsIcon } from '../icons'
 
 export const QuickstartGuide = () => {
   const frameworks = [
-    { name: 'Next.js', icon: NextJsIcon, slug: 'templates/react/next-js' },
-    { name: 'Solid Start', icon: SolidStartIcon, slug: 'templates/solid/solid-start' },
-    { name: 'Nuxt', icon: NuxtIcon, slug: 'templates/vue/nuxt' },
+    { name: 'Next.js', icon: NextJsIcon, slug: 'park-ui-nextjs' },
+    // { name: 'Solid Start', icon: SolidStartIcon, slug: 'templates/solid/solid-start' },
+    // { name: 'Nuxt', icon: NuxtIcon, slug: 'templates/vue/nuxt' },
   ]
   return (
     <Wrap direction="row" gap={{ base: '4', md: '6' }} className="not-prose">
       {frameworks.map(({ name, icon: Icon, slug }) => (
         <a
           key={name}
-          href={`https://stackblitz.com/github/cschroeter/park-ui/tree/main/${slug}`}
+          href={`https://stackblitz.com/~/github.com/cschroeter/${slug}`}
           target="_blank"
           rel="noreferrer"
           className={link}
