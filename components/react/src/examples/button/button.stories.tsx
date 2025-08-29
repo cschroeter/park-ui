82e368dee6a247252f7f121ec243ef4fb8547a21
Christian Busch <dev@grizzly.codes>
2025-08-29 20:46:30 +0200

    document more button examples

--- components/react/src/examples/button/button.stories.tsx
+++ components/react/src/examples/button/button.stories.tsx
@@ -7,7 +7,11 @@ const meta: Meta = {
 
 export default meta
 
+export { App as asLink } from './as-link'
 export { App as basic } from './basic'
 export { App as colors } from './colors'
+export { App as disabled } from './disabled'
+export { App as icon } from './icon'
+export { App as ref } from './ref'
 export { App as sizes } from './sizes'
 export { App as variants } from './variants'
