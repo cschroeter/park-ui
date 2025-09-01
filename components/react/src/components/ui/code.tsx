93fab246b1f7cc1d0653e0e1ca457640030f3d82
Christian Schröter <dev@grizzly.codes>
2025-09-01 22:13:17 +0200

    resolve a11y issue

--- components/react/src/components/ui/code.tsx
+++ components/react/src/components/ui/code.tsx
@@ -4,4 +4,4 @@ import { styled } from 'styled-system/jsx'
 import { code } from 'styled-system/recipes'
 
 export type CodeProps = ComponentProps<typeof Code>
-export const Code = styled(ark.div, code)
+export const Code = styled(ark.code, code)
