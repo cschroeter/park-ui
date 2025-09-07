c1e1047c38dc6909117d827ed30be8086fa96291
Christian Schröter <dev@grizzly.codes>
2025-09-07 21:02:12 +0200

    extend code doc

--- components/react/src/examples/button/colors.tsx
+++ components/react/src/examples/button/colors.tsx
@@ -4,10 +4,10 @@ import { Button } from '@/components/ui'
 export const App = () => {
   return (
     <Wrap gap="4">
-      <Button colorPalette="red">Button</Button>
-      <Button colorPalette="green">Button</Button>
       <Button colorPalette="blue">Button</Button>
+      <Button colorPalette="green">Button</Button>
       <Button colorPalette="amber">Button</Button>
+      <Button colorPalette="red">Button</Button>
     </Wrap>
   )
 }
--- components/react/src/examples/code/code.stories.tsx
+++ components/react/src/examples/code/code.stories.tsx
@@ -7,4 +7,6 @@ const meta: Meta = {
 export default meta
 
 export { App as basic } from './basic'
+export { App as colors } from './colors'
 export { App as sizes } from './sizes'
+export { App as variants } from './variants'
