41274d8d5378244cad7b1c8d22ccc251872470fa
Christian Schröter <dev@grizzly.codes>
2025-09-10 19:53:19 +0200

    wip

--- packages/preset/src/colors/teal.ts
+++ packages/preset/src/colors/teal.ts
@@ -25,8 +25,65 @@ export const teal = defineSemanticTokens.colors({
   a10: { value: { _light: '#009684f2', _dark: '#0dffe0ae' } },
   a11: { value: { _light: '#008573', _dark: '#0afed5d6' } },
   a12: { value: { _light: '#00332df2', _dark: '#b8ffebef' } },
-  contrast: { value: { _light: 'white', _dark: 'white' } },
-  fg: { value: { _light: '{colors.teal.a11}', _dark: '{colors.teal.a11}' } },
-  solid: { value: { _light: '{colors.teal.9}', _dark: '{colors.teal.9}' } },
-  emphasiszed: { value: { _light: '{colors.teal.10}', _dark: '{colors.teal.10}' } },
+
+  solid: {
+    bg: {
+      DEFAULT: { value: { _light: '{colors.teal.9}', _dark: '{colors.teal.9}' } },
+      hover: { value: { _light: '{colors.teal.10}', _dark: '{colors.teal.10}' } },
+      active: { value: { _light: '{colors.teal.11}', _dark: '{colors.teal.11}' } },
+    },
+    fg: {
+      DEFAULT: { value: { _light: 'white', _dark: 'white' } },
+    },
+  },
+  subtle: {
+    bg: {
+      DEFAULT: { value: { _light: '{colors.teal.a3}', _dark: '{colors.teal.a3}' } },
+      hover: { value: { _light: '{colors.teal.a4}', _dark: '{colors.teal.a4}' } },
+      active: { value: { _light: '{colors.teal.a5}', _dark: '{colors.teal.a5}' } },
+    },
+    fg: {
+      DEFAULT: { value: { _light: '{colors.teal.a11}', _dark: '{colors.teal.a11}' } },
+    },
+  },
+  surface: {
+    bg: {
+      DEFAULT: { value: { _light: '{colors.teal.a2}', _dark: '{colors.teal.a2}' } },
+      hover: { value: { _light: '{colors.teal.a3}', _dark: '{colors.teal.a3}' } },
+      active: { value: { _light: '{colors.teal.a4}', _dark: '{colors.teal.a4}' } },
+    },
+    fg: {
+      DEFAULT: { value: { _light: '{colors.teal.a11}', _dark: '{colors.teal.a11}' } },
+    },
+    border: {
+      DEFAULT: { value: { _light: '{colors.teal.a5}', _dark: '{colors.teal.a5}' } },
+      hover: { value: { _light: '{colors.teal.a6}', _dark: '{colors.teal.a6}' } },
+      active: { value: { _light: '{colors.teal.a6}', _dark: '{colors.teal.a6}' } },
+    },
+  },
+  outline: {
+    bg: {
+      DEFAULT: { value: { _light: 'transparent', _dark: 'transparent' } },
+      hover: { value: { _light: '{colors.teal.a2}', _dark: '{colors.teal.a2}' } },
+      active: { value: { _light: '{colors.teal.a3}', _dark: '{colors.teal.a3}' } },
+    },
+    fg: {
+      DEFAULT: { value: { _light: '{colors.teal.a11}', _dark: '{colors.teal.a11}' } },
+    },
+    border: {
+      DEFAULT: { value: { _light: '{colors.teal.a7}', _dark: '{colors.teal.a7}' } },
+      hover: { value: { _light: '{colors.teal.a8}', _dark: '{colors.teal.a8}' } },
+      active: { value: { _light: '{colors.teal.a8}', _dark: '{colors.teal.a8}' } },
+    },
+  },
+  plain: {
+    bg: {
+      DEFAULT: { value: { _light: 'transparent', _dark: 'transparent' } },
+      hover: { value: { _light: '{colors.teal.a3}', _dark: '{colors.teal.a3}' } },
+      active: { value: { _light: '{colors.teal.a4}', _dark: '{colors.teal.a4}' } },
+    },
+    fg: {
+      DEFAULT: { value: { _light: '{colors.teal.a11}', _dark: '{colors.teal.a11}' } },
+    },
+  },
 })
