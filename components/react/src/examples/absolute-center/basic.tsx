9e904c05aefd92d55c090b52d6c071db2b61eee3
Christian Busch <dev@grizzly.codes>
2025-09-26 21:40:01 +0200

    update absolute center example

--- components/react/src/examples/absolute-center/basic.tsx
+++ components/react/src/examples/absolute-center/basic.tsx
@@ -3,9 +3,9 @@ import { AbsoluteCenter } from '@/components/ui'
 
 export const App = () => {
   return (
-    <Box position="relative" height="200px" bg="gray.100" borderRadius="md">
+    <Box position="relative" height="40">
       <AbsoluteCenter>
-        <Box bg="white" p="4" borderRadius="md" shadow="md">
+        <Box bg="gray.surface.bg" p="4" borderRadius="l2" boxShadow="md">
           Centered Content
         </Box>
       </AbsoluteCenter>
