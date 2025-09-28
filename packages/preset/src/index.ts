e26b8edbaf39c57a04fa9d3a5f56dbc5bc946052
Christian Busch <dev@grizzly.codes>
2025-09-28 22:47:13 +0200

    improve hover condition

--- packages/preset/src/index.ts
+++ packages/preset/src/index.ts
@@ -36,7 +36,7 @@ export const preset = definePreset({
     extend: {
       light: ':root &, .light &',
       invalid: '&:is(:user-invalid, [data-invalid], [aria-invalid=true])',
-      hover: '&:not(:disabled):hover',
+      hover: '&:not(:is(:disabled, [data-state=on], :active)):hover',
       active: '&:not(:disabled):active',
       checked: '&:is(:checked, [data-checked], [aria-checked=true], [data-state=indeterminate])',
       on: '&:is([data-state=on])',
