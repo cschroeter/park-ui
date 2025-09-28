329904adc551af308bc793c35677812394e1897d
Christian Busch <dev@grizzly.codes>
2025-09-28 21:43:14 +0200

    revise splitter

--- components/react/src/examples/splitter/collapsible.tsx
+++ components/react/src/examples/splitter/collapsible.tsx
@@ -2,7 +2,13 @@ import { Splitter } from '@/components/ui'
 
 export const App = () => {
   return (
-    <Splitter.Root panels={[{ id: 'a' }, { id: 'b' }]}>
+    <Splitter.Root
+      defaultSize={[15, 20]}
+      panels={[
+        { id: 'a', collapsible: true, collapsedSize: 5, minSize: 25, maxSize: 25 },
+        { id: 'b', minSize: 50 },
+      ]}
+    >
       <Splitter.Panel id="a">A</Splitter.Panel>
       <Splitter.ResizeTrigger id="a:b" aria-label="Resize" />
       <Splitter.Panel id="b">B</Splitter.Panel>
--- components/react/src/examples/splitter/splitter.stories.tsx
+++ components/react/src/examples/splitter/splitter.stories.tsx
@@ -7,3 +7,7 @@ const meta: Meta = {
 export default meta
 
 export { App as basic } from './basic'
+export { App as collapsible } from './collapsible'
+export { App as multiple } from './multiple'
+export { App as store } from './store'
+export { App as vertical } from './vertical'
