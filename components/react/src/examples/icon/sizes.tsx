6f816b2268d3a706454e04a23578ccc17b9f5182
Christian Busch <dev@grizzly.codes>
2025-10-04 00:26:32 +0200

    revise icon recipe

--- components/react/src/examples/icon/sizes.tsx
+++ components/react/src/examples/icon/sizes.tsx
@@ -3,7 +3,7 @@ import { Wrap } from 'styled-system/jsx'
 import { Icon } from '@/components/ui'
 
 export const App = () => {
-  const sizes = ['xs', 'sm', 'md', 'lg', 'xl', '2xl'] as const
+  const sizes = ['xs', 'sm', 'md', 'lg', 'xl'] as const
   return (
     <Wrap gap="4">
       {sizes.map((size) => (
--- packages/preset/src/recipes/icon.ts
+++ packages/preset/src/recipes/icon.ts
@@ -14,30 +14,12 @@ export const icon = defineRecipe({
   },
   variants: {
     size: {
-      xs: {
-        w: '3',
-        h: '3',
-      },
-      sm: {
-        w: '4',
-        h: '4',
-      },
-      md: {
-        w: '5',
-        h: '5',
-      },
-      lg: {
-        w: '6',
-        h: '6',
-      },
-      xl: {
-        w: '7',
-        h: '7',
-      },
-      '2xl': {
-        w: '8',
-        h: '8',
-      },
+      '2xs': { boxSize: '3' },
+      xs: { boxSize: '4' },
+      sm: { boxSize: '4.5' },
+      md: { boxSize: '5' },
+      lg: { boxSize: '5.5' },
+      xl: { boxSize: '6' },
     },
   },
 })
