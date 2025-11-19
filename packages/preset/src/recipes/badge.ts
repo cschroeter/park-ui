844d633a3b3df5fe0a03a3aac4d8c87f566b7bb2
Christian Busch <dev@grizzly.codes>
2025-11-19 23:37:18 +0100

    add badge to legacy version

--- packages/preset/src/recipes/badge.ts
+++ packages/preset/src/recipes/badge.ts
@@ -43,6 +43,7 @@ export const badge = defineRecipe({
       md: { fontSize: 'xs', px: '2', h: '5', gap: '1', _icon: { boxSize: '3' } },
       lg: { fontSize: 'xs', px: '2.5', h: '5.5', gap: '1', _icon: { boxSize: '3.5' } },
       xl: { fontSize: 'sm', px: '2.5', h: '6', gap: '1.5', _icon: { boxSize: '4' } },
+      '2xl': { fontSize: 'md', px: '3', h: '7', gap: '1.5', _icon: { boxSize: '4.5' } },
     },
   },
 })
--- website/src/components/marketing/hero.tsx
+++ website/src/components/marketing/hero.tsx
@@ -1,6 +1,7 @@
 import { ArrowRightIcon } from 'lucide-react'
+import NextLink from 'next/link'
 import { Box, HStack, Stack } from 'styled-system/jsx'
-import { Heading, Text } from '@/components/ui'
+import { Badge, Heading, Text } from '@/components/ui'
 import { ThemeDrawer } from '../docs/theme-drawer'
 import { ReactIcon, SolidIcon } from '../icons'
 import { PageButton } from '../navigation/page-button'
@@ -11,9 +12,17 @@ export const Hero = () => {
     <HeroGroup>
       <HeroItem>
         <Stack gap={{ base: '4', md: '6' }} align="flex-start">
-          <Heading as="h1" textStyle={{ base: '4xl', md: '6xl' }} fontWeight="bold">
-            Build your own Design System
-          </Heading>
+          <Stack gap="4">
+            <NextLink href="https://legacy.park-ui.com">
+              <Badge size="2xl" variant="subtle">
+                Need the old version? Click here
+                <ArrowRightIcon />
+              </Badge>
+            </NextLink>
+            <Heading as="h1" textStyle={{ base: '4xl', md: '6xl' }} fontWeight="bold">
+              Build your own Design System
+            </Heading>
+          </Stack>
           <Text textStyle={{ base: 'lg', md: 'xl' }} color="fg.muted" maxW="2xl">
             Beautifully designed components built with Ark UI and Panda CSS that work with a variety
             of JS frameworks.
