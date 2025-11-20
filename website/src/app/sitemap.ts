f83b770cd212270e43e9d618124ae726019ea636
Christian Busch <dev@grizzly.codes>
2025-11-20 20:38:54 +0100

    fix(website): add changelog to sitemap

--- website/src/app/sitemap.ts
+++ website/src/app/sitemap.ts
@@ -3,12 +3,16 @@ import type { MetadataRoute } from 'next'
 
 const baseUrl = 'https://park-ui.com'
 
+const staticPages = ['/', '/changelog']
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const staticEntries = staticPages.map((page) => ({
+    url: new URL(page, baseUrl).toString(),
+  }))
+
   const docPages = docs.map((doc) => ({
     url: new URL(doc.href, baseUrl).toString(),
   }))
 
-  const sitemapEntries = [{ url: baseUrl }, ...docPages]
-
-  return sitemapEntries
+  return [...staticEntries, ...docPages]
 }
