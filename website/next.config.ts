64cec246c5e9cb3b4fcb00e81c5f312e5ca77c0b
Christian Busch <dev@grizzly.codes>
2025-12-01 18:25:32 +0100

    ci: include static files from public folder

--- website/next.config.ts
+++ website/next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     optimizePackageImports: ['@ark-ui/react'],
   },
   outputFileTracingIncludes: {
-    '/*': ['../components/*/src/**/*'],
+    '/*': ['../components/*/src/**/*', './public/registry/**/*', './public/schema/**/*'],
   },
   output: 'standalone',
 
